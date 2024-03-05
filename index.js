d2ae1a36a993c36ba0872ca7205aeecf9aedad21
anoosha-everest <160118595+anoosha-everest@users.noreply.github.com>
2024-03-05 12:38:11 +0530

    adding files

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const col=document.querySelector(".name");
-function red(){
-    console.log("hello");
-    col.innerHTML="Red";
-    col.style.color="red";
+function show(tabId){
+    const t=document.getElementsByClassName("tab-content");
+    for (const tab of t){
+        tab.style.display="none";
+    }
+    document.getElementById(tabId).style.display="flex";
 }
-function green(){
-    console.log("hello");
-    col.innerHTML="Green";
-    col.style.color="green";
-}
-function blue(){
-    console.log("hello");
-    col.innerHTML="Blue";
-    col.style.color="blue";
-}
-function yellow(){
-    console.log("hello");
-    col.innerHTML="Yellow";
-    col.style.color="yellow";
-}
+
+const form=document.getElementById('form');
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    const formData=new FormData(event.target);
+    const name=formData.get('name');
+    const pass=formData.get('password');
+    const email=formData.get('email');
+    console.log("form",name,pass,email);
+    // console.log("name",document.getElementById('name').value);
+
+    var result = document.createElement('div');
+    result.innerHTML = '<h2>Submitted Form Data:</h2>' +"Name="+ (name) +" password="+ (pass)+" email="+(email);
+            document.body.appendChild(result);
+});
+console.log("hello");
